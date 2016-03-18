5de436c1e27ef9b55ac7e124f9c93c37164a1082
P Davis <pd3045@tiscali.co.uk>
2016-03-18 14:49:26 +0000

    work templates

--- src/java/com/utl/MainBean.java
+++ src/java/com/utl/MainBean.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.firstapp;
+package com.utl;
 
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
